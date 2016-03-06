e96d61c1b838a9e8bbb5e267bc07d6d8e109903e
anirudt <anirudt@gmail.com>
2016-03-06 21:28:19 +0530

    printenv: implemented usual functionality, no args yet

--- utils/printenv.cpp
+++ utils/printenv.cpp
@@ -1,15 +1,33 @@
 #include <stdlib.h>
 #include <cstdio>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char **argv, char **envp) {
+  
   char **env;
-  for (env = envp; *env != 0; env++) {
-    char *environ = *env;
-    cout << environ << endl;
+
+  if (argc > 1) {
+    for (int i = 1; i < argc ; i++) {
+      string s = argv[i];
+      for (env = envp; *env != 0; env++) {
+        string env_entry = *env;
+        if (env_entry.find(s) != -1) {
+          cout << env_entry << endl;
+          break;
+        }
+      }
+    }
   }
 
+  else {
+    /* Handling in case of no argument being passed */
+    for (env = envp; *env != 0; env++) {
+      char *environ = *env;
+      cout << environ << endl;
+    }
+  }
   return 0;
 }
