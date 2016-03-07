539bfae9bfc4912a62e2c3e8a7056054b9667b2c
anirudt <anirudt@gmail.com>
2016-03-07 10:59:18 +0530

    printenv: working getopt_long, and all funct except 0 byte padding

--- utils/printenv.cpp
+++ utils/printenv.cpp
@@ -12,20 +12,37 @@ using namespace std;
 
 int main(int argc, char **argv, char **envp) {
   
-const struct option long_options[] = {
-  {"version", no_argument, 0, 'v'},
-  {"help",    no_argument, 0, 'h'},
-  {"null",    no_argument, 0, '0'},
-  {0, 0, 0, 0}
-};
+  const struct option long_options[] = {
+    {"version", no_argument, 0, 'v'},
+    {"help",    no_argument, 0, 'h'},
+    {"null",    no_argument, 0, '0'},
+    {0, 0, 0, 0}
+  };
   char **env;
   int option_index = 0;
 
-  int c = getopt_long(argc, argv, "vhn", long_options, &option_index);
-  cout << c << endl;
+  int c = getopt_long(argc, argv, "vh0", long_options, &option_index);
 
-  if (argc > 1) {
-    for (int i = 1; i < argc ; i++) {
+  /* Preventing error messages */
+  opterr = 0;
+
+  switch(c) {
+    case 'v': cout << "printenv: The version is 1.0.1" << endl;
+              break;
+
+    case 'h': cout << "Use 'man printenv' to know how to use this tool" << endl;
+              break;
+
+    case '0': /* TODO */
+              cout << "Ending each line with byte 0, rather than newline" << endl;
+              break;
+
+    case '?': break;
+  }
+
+  if (optind < argc) {
+    /* Processing any left non-option arguments */
+    for (int i = optind; i < argc ; i++) {
       string s = argv[i];
       for (env = envp; *env != 0; env++) {
         string env_entry = *env;
@@ -36,8 +53,7 @@ const struct option long_options[] = {
       }
     }
   }
-
-  else {
+  else if(argc == 1) {
     /* Handling in case of no argument being passed */
     for (env = envp; *env != 0; env++) {
       char *environ = *env;
