2c441f08c4bcfca44400791dd302e9832e0e5695
anirudt <anirudt@gmail.com>
2016-03-08 16:32:39 +0530

    added echo framework, and added echo to Makefile

--- Makefile
+++ Makefile
@@ -6,10 +6,13 @@ SOURCE_DIR=utils
 
 BUILD_DIR=bin
 
-all: printenv
+all: printenv echo
 
 printenv:
 	$(CC) $(SOURCE_DIR)/printenv.cpp -o $(BUILD_DIR)/printenv
+
+echo: 
+	$(CC) $(SOURCE_DIR)/echo.cpp -o $(BUILD_DIR)/echo
 
 clean:
 	rm -rf $(BUILD_DIR)/*
--- utils/echo.cpp
+++ utils/echo.cpp
@@ -9,23 +9,50 @@
 
 using namespace std;
 
+/* TODO 
+ *
+ * Make a function to 
+ * strip double and single
+ * quotes.
+ *
+ */
 
 int main(int argc, char **argv) {
  
   const struct option long_options[] = {
-    {"version", no_argument, 0, 0},
-    {"help",    no_argument, 0, 0},
-    {0,         no_argument, 0, 'e'},
-    {0,         no_argument, 0, 'E'},
-    {0.         no_argument, 0, 'n'},
+    {"version", no_argument, 0, 'v'},
+    {"help",    no_argument, 0, 'h'},
+    {0,         required_argument, 0, 'e'},
+    {0,         required_argument, 0, 'E'},
+    {0,         required_argument, 0, 'n'},
     {0,         0,           0, 0}
   };
 
   int option_index = 0;
 
-  int c = getopt_long(argc, argv, "", long_options, &option_index);
+  int c = getopt_long(argc, argv, "vhe:E:n:", long_options, &option_index);
+  cout << option_index << endl;
 
   /* Preventing error messages */
   opterr = 0;
+
+  switch(c) {
+    case 'v': cout << "echo: The version is 1.0.1" << endl;
+              break;
+
+    case 'h': cout << "Refer the man page for more details" << endl;
+              break;
+
+    case 'e': break;
+
+    case 'E': break;
+
+    case 'n': break;
+
+    case '?': break;
+
+    case -1: break;
+  }
+  
   return 0;
 }
