3dd00d6d43c0da3c7d7d720e4d5ea0bb2f276b87
anirudt <anirudt@gmail.com>
2016-03-10 15:21:33 +0530

    wc: modularised working

--- utils/wc.cpp
+++ utils/wc.cpp
@@ -11,43 +11,88 @@
 
 using namespace std;
 
+int line_count(char *s) {
+  int line_count = 0;
+  ifstream fin(s, ios::in); string line;
+  while(getline(fin, line)) {
+    line_count++;
+  }
+  return line_count;
+}
+
+int char_count(char *s) {
+  int char_count = 0;
+  ifstream fin(s, ios::in); char tmp;
+  fin.get( tmp );
+  while(!fin.eof()) {
+    fin.get( tmp );
+    char_count++;
+  }
+  return char_count;
+}
+
+int stat_count(bool byte_counter, bool char_counter,
+    bool line_counter, bool get_max_line,
+    bool word_counter, bool read_master_file, char **argv, int optind, int argc) {
+  /* For each file, DO */
+  for (int i = optind; i < argc; i++) {
+    int byte_cnt, char_cnt, line_cnt, max_line, word_cnt, master_file;
+    if (byte_counter) {
+    }
+    if (char_counter) {
+      char_cnt = char_count(argv[i]);
+    }
+    if (line_counter) {
+      line_cnt = line_count(argv[i]);
+    }
+    if (get_max_line) {
+    }
+    if (word_counter) {
+    }
+    if (read_master_file) {
+    }
+    cout << char_cnt << " " << line_cnt << " " << argv[i] << endl;
+  }
+}
+
 int main(int argc, char **argv) {
   const struct option long_options[] = {
-  {"bytes", required_argument, 0, 'c'},
-  {"chars", required_argument, 0, 'm'},
-  {"lines", required_argument, 0, 'l'},
-  {"files0-from", required_argument, 0, 'f'},
-  {"max-line-length", required_argument, 0, 'x'},
-  {"words", required_argument, 0, 'w'},
-  {"version", no_argument, 0, 'v'},
-  {"help", no_argument, 0, 'h'}
+    {"bytes", required_argument, 0, 'c'},
+    {"chars", required_argument, 0, 'm'},
+    {"lines", required_argument, 0, 'l'},
+    {"files0-from", required_argument, 0, 'f'},
+    {"max-line-length", required_argument, 0, 'L'},
+    {"words", required_argument, 0, 'w'},
+    {"version", no_argument, 0, 'v'},
+    {"help", no_argument, 0, 'h'}
   }; 
 
   int option_index = 0;
+  bool byte_counter = false, char_counter = false,
+       line_counter = false, get_max_line = false,
+       word_counter = false, read_master_file = false;
 
   int c;
-  while((c = getopt_long(argc, argv, "cmlfxwvh", long_options, &option_index))!=-1) {
+  while((c = getopt_long(argc, argv, "cmlfLwvh", long_options, &option_index))!=-1) {
     switch(c) {
-      case 'c': for (int i=optind; i<argc; i++) {
-                  int char_count = 0;
-                  ifstream fin(argv[i], ios::in); char tmp;
-                  fin.get( tmp );
-                  while(!fin.eof()) {
-                    fin.get( tmp );
-                    char_count++;
-                  }
-                  cout << char_count << " " << argv[i] << endl;
-                }
+      case 'c': byte_counter = true;
                 break;
-      case 'm': break;
-      case 'l': break;
-      case 'f': break;
-      case 'x': break;
-      case 'w': break;
+      case 'm': char_counter = true;
+                break;
+      case 'l': line_counter = true;
+                break;
+      case 'f': read_master_file = true;
+                break;
+      case 'L': get_max_line = true;
+                break;
+      case 'w': word_counter = true;
+                break;
       case '?': break;
       case -1: break;
     }
   }
+  stat_count(byte_counter, char_count, line_counter, get_max_line, word_counter, read_master_file, argv, optind, argc);
+
 
   return 0;
 } 
