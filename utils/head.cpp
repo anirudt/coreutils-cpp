f96690b02a84289e35b02f1e79a2542d25d494b8
Anirud <anirudt@gmail.com>
2016-03-25 19:29:14 +0530

    head: added char_counting feature, untested

--- utils/head.cpp
+++ utils/head.cpp
@@ -20,12 +20,27 @@ void print_lines(char *s, int n) {
   }
 }
 
-void process(int argc, char **argv, int optind, bool line_print, int line_print_arg) {
+void print_chars(char *s, int n) {
+  ifstream fin(s, os::in); char tmp;
+  int char_count = 0;
+  fin.get(tmp);
+  while(!fin.eof() && char_count < n) {
+    fin.get( tmp );
+    cout << tmp;
+  }
+}
+
+void process(int argc, char **argv, int optind, bool line_print, int line_print_arg,
+             bool char_print, int char_print_arg) {
   for (int i = optind; i < argc; i++) {
     cout << "==> " << argv[i] << " <==" << endl;
     if (line_print) {
       print_lines(argv[i], line_print_arg);
     }
+    if (char_print) {
+      print_chars(argv[i], char_print_arg);
+    }
+    cout << endl;
   }
 }
 
@@ -42,20 +57,23 @@ int main(int argc, char **argv) {
   int c;
 
   // Indicators
-  bool line_print = false;
-  int line_print_arg = 0;
+  bool line_print = false, char_print = false;
+  int line_print_arg = 0, char_print_arg = 0;
   while((c = getopt_long(argc, argv, "c:n:qvhk", long_options, &option_index)) != -1) {
     switch(c) {
       case 'n': line_print = true;
                 line_print_arg = stoi(optarg);
                 break;
+      case 'c': char_print = true;
+                char_count = stoi(optarg);
     }
   }
   if (!line_print) {
-    process(argc, argv, optind, line_print, 10);
+    process(argc, argv, optind, line_print, 10, false, 0);
   }
   else {
-    process(argc, argv, optind, line_print, line_print_arg);
+    process(argc, argv, optind, line_print, line_print_arg,\
+            char_print, char_print_arg);
   }
   return 0;
 }
