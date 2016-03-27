4a0cdea660762ba13b000b71a181f6c9965e15cd
anirudt <anirudt@gmail.com>
2016-03-27 14:28:04 +0530

    utilities: added multipler

--- utils/utilities.cpp
+++ utils/utilities.cpp
@@ -11,10 +11,15 @@ typdef struct memory {
 
 typedef long long int lli;
 
-char s[17] = {"b", "kB", "K", "MB", "M", "GB", "G",
-              "TB", "T", "PB", "P", "EB", "E", "ZB",
-              "Z", "YB", "Y"};
+char s[7] = {"b", "kB", "K", "MB", "M", "GB", "G"};
+//              "TB", "T", "PB", "P", "EB", "E", "ZB",
+//              "Z", "YB", "Y"};
+
+lli multiplier[7] = {512, 1000, 1024, 1000*1000, 1024*1024,
+                      1000*1000*1000, 1024*1024*1024};
 
 lli mem_parse(char *s) {
+  /*Parses memory argument to provide 
+   * a number and a multipler*/
    
 }
